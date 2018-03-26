7d0f8816a3122569b5c11fdca67b2b08930ec49c
Eliemer <eliemer619@gmail.com>
2018-03-26 12:33:04 -0400

    Made blank DAO's and handlers. Made GroupChatDAO

---
+++ dao/GroupChatDAO.py
@@ -0,0 +1,53 @@
+class GroupChatDAO:
+
+    def __init__(self):
+        self.groups = []
+        G1 = [1982, 'Wannabes', 158, '03-04-2017']
+        G2 = [870, 'ININ4010', 584, '02-17-2018']
+        G3 = [137, 'Friend\'s Club', 265, '08-27-2015']
+        G4 = [1087, 'ICOM 5016 - Databases', 265, '02-14-2018']
+        G5 = [497, 'Pedrito\'s personal hell', 584, '08-29-2017']
+        G6 = [798, 'Batman\'s Fanclub', 965, '07-11-2013']
+        self.groups.append(G1)
+        self.groups.append(G2)
+        self.groups.append(G3)
+        self.groups.append(G4)
+        self.groups.append(G5)
+        self.groups.append(G6)
+
+    def getGroups(self):
+        return self.groups
+
+    def getGroupByName(self, name):
+        groups = []
+        for g in self.groups:
+            if name == g[1]:
+                groups.append(g)
+        return groups
+
+    def getGroupByID(self, gid):
+        group = []
+        for g in self.groups:
+            if gid == g[0]:
+                group.append(g)
+        return group
+
+    def getGroupAdmin(self, gid):
+        admin = []
+        for g in self.groups:
+            if gid == g[0]:
+                admin.append(g[2])
+        return admin
+
+    def getGroupDate(self, gid):
+        date = []
+        for g in self.groups:
+            if gid == g[0]:
+                date.append(g[3])
+        return date
+
+    def getGroupNames(self):
+        group = []
+        for g in self.groups:
+            group.append(g[1])
+        return group
