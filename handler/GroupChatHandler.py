64af6b74390c973a8960a1911760abd797851ac9
Eliemer <eliemer619@gmail.com>
2018-03-26 12:52:26 -0400

    Made GroupChatHandler

---
+++ handler/GroupChatHandler.py
@@ -0,0 +1,49 @@
+from dao.GroupChatDAO import GroupChatDAO
+from flask import *
+
+class GroupChatHandler:
+
+
+    def arrange(self, row):
+        groups = []
+        groups['group_id'] = row[0]
+        groups['group_name'] = row[1]
+        groups['group_admin'] = row[2]
+        groups['date_created'] = row[3]
+        return groups
+
+    def getGroups(self):
+        dao = GroupChatDAO()
+        result = dao.getGroups()
+        groups = []
+        for i in result:
+            groups.append(self.arrange(i))
+        return jsonify(Groups=groups)
+
+    def getGroupByName(self, name):
+        dao = GroupChatDAO()
+        result = dao.getGroupByName(name)
+        if result is None:
+            return jsonify(ERROR='No group found by that name')
+        return jsonify(Groups=result)
+
+    def getGroupByID(self, gid):
+        dao = GroupChatDAO()
+        result = dao.getGroupByID(gid)
+        if result is None:
+            return jsonify(ERROR='No group found with that ID')
+        return result
+
+    def getGroupAdmin(self, gid):
+        dao = GroupChatDAO()
+        result = dao.getGroupAdmin(gid)
+        if result is None:
+            return jsonify(ERROR='No group found with that ID')
+        return result
+
+    def getGroupNames(self):
+        dao = GroupChatDAO()
+        result = dao.getGroupNames()
+        if result is None:
+            return jsonify(ERROR='No group found')
+        return result
