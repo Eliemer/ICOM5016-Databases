48d411b20328759b673d304d4f5fd85285532960
Eliemer <eliemer619@gmail.com>
2018-03-27 22:52:29 -0400

    UserHandler, GroupChat, Messages arrange functions. Updated Messages DAO

--- handler/MessagesHandler.py
+++ handler/MessagesHandler.py
@@ -3,19 +3,46 @@ from dao.MessagesDAO import *
 
 
 class MessagesHandler:
-    def sort(self, row):
+    def arrange(self, row):
         messages = {}
-        messages['id'] = row[0]
-        messages['name'] = row[1]
-        messages['message'] = row[2]
+        messages['message_id'] = row[0]
+        messages['sender_id'] = row[1]
+        messages['group_id'] = row[2]
+        messages['date_sent'] = row[3]
+        messages['message_content'] = row[4]
         return messages
+
+    def arrangeMessageID(self, row):
+        message = {}
+        message['message_id'] = row[0]
+        return message
+
+    def arrangeMessageSenderID(self, row):
+        message = {}
+        message['sender_id'] = row[1]
+        return message
+
+    def arrangeMessageGroupID(self, row):
+        message = {}
+        message['group_id'] = row[2]
+        return message
+
+    def arrangeMessageDateSent(self, row):
+        message = {}
+        message['date_sent'] = row[3]
+        return message
+
+    def arrangeMessageSenderID(self, row):
+        message = {}
+        message['message_content'] = row[4]
+        return message
 
     def getMessages(self):
         dao = MessagesDAO()
         result = dao.getMessages()
         message=[]
         for i in result:
-            message.append(self.sort(i))
+            message.append(self.arrange(i))
         return jsonify(Messages=message)
 
     def getUserMessagesById(self, usrid):
@@ -25,5 +52,5 @@ class MessagesHandler:
         if result is None:
             return jsonify(Error='Id not in Database')
         for i in result:
-            message.append(self.sort(i))
+            message.append(self.arrange(i))
         return jsonify(Message=message)
